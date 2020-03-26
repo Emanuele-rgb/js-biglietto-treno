02cd84485f411a07ea5d4b92391399302a2f4e03
Emanuele Sanquirico <manu.sanquirico@gmail.com>
2020-03-26 18:55:21 +0100

    tofixed missing

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var km;
 var eta;
-var prezzoprov
+var prezzoprov;
 var prezzo;
 
 
@@ -12,16 +12,20 @@ prezzoprov = km*0.21;
 
 
 if (eta < 18) {
-  console.log("20% di sconto");
+  console.log(sconto ="20% di sconto");
   console.log(prezzo=(prezzoprov*0.8));
 }
 
 else if (eta > 65) {
-  console.log("40% di sconto");
+  console.log(sconto = "40% di sconto");
   console.log(prezzo=(prezzoprov*0.6));
 }
 
 else {
-  console.log("nessuno sconto");
+  console.log(sconto = "nessuno sconto");
   console.log(prezzo=prezzoprov);
 }
+
+
+document.getElementById('prezzo').innerHTML = prezzo;
+document.getElementById('sconto').innerHTML = sconto;
